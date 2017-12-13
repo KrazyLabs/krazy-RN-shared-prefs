def28218bfda7abeb4106b29d147db7c077088fd
IANBRUNE <ianbrune92@gmail.com>
2017-12-13 14:12:15 -0700

    Update build.gradle to use new tool version

--- android/src/main/java/com/reactlibrary/RNKrazyRnSharedPrefsModule.java
+++ android/src/main/java/com/reactlibrary/RNKrazyRnSharedPrefsModule.java
@@ -6,6 +6,12 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+
 public class RNSharedPrefsModule extends ReactContextBaseJavaModule {
 
   private final ReactApplicationContext reactContext;
