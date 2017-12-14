8fc952aaa216cce0bdc48e80653a8efa13e307db
IANBRUNE <ianbrune92@gmail.com>
2017-12-14 16:05:31 -0700

    Still working on react context initializing the correct workspace. Definitely some magic going on here that causes it to work differenty than the normal ApplicationContext

--- android/src/main/java/com/reactlibrary/RNSharedPrefsModule.java
+++ android/src/main/java/com/reactlibrary/RNSharedPrefsModule.java
@@ -13,15 +13,38 @@ import android.content.SharedPreferences.Editor;
 public class RNSharedPrefsModule extends ReactContextBaseJavaModule {
 
   private final ReactApplicationContext reactContext;
+  private final SharedPreferences sharedPrefs;
+  private final ReactApplicationContext customContext;
 
   public RNSharedPrefsModule(ReactApplicationContext reactContext) {
     super(reactContext);
     this.reactContext = reactContext;
+    this.customContext = null;
+    this.sharedPrefs = null;
   }
 
-  public void setSharedPreference(String value) {
-      SharedPreferences prefs = getApplicationContext().getSharedPreferences("sharedprefone", Context.MODE_WORLD_READABLE);
+  public void init (String packageContext) {
+    this.customContext = packageContext;
+    if(this.customContext != null) {
+        Context context = this.reactContext.getApplicationContext().createPackageContext(this.customContext, Context.MODE_WORLD_WRITEABLE);
+        this.sharedPrefs = context.getSharedPreferences("KCL-prefs", Context.MODE_WORLD_READABLE);
+    } else {
+        this.sharedPrefs = this.reactContext.getApplicationContext().getSharedPreferences("KCL-prefs", Context.MODE_WORLD_READABLE)
+    }
   }
+
+
+  public void setSharedPreference(String key, String value) {
+//      SharedPreferences prefs = getApplicationContext().getSharedPreferences("sharedprefone", Context.MODE_WORLD_READABLE);
+      Editor edit = this.sharedPrefs.edit();
+      edit.putString(key, value);
+    edit.commit();
+  }
+
+    public string getSharedPreference(String key) {
+//        SharedPreferences prefs = getApplicationContext().getSharedPreferences("sharedprefone", Context.MODE_WORLD_READABLE);
+        return this.sharedPrefs.getString(key,"Couldn't find key");
+    }
 
   @Override
   public String getName() {
